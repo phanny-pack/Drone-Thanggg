1e1293a29861945d715a2f0acc1ed48ef70536d2
9KelvinN <9kelvin@gmail.com>
2021-01-23 12:37:02 -0800

    Added basis code to IMU-Logging.py

--- data-logging/IMU-logging.py
+++ data-logging/IMU-logging.py
@@ -1,1 +1,15 @@
+#Documentation: https://github.com/adafruit/Adafruit_CircuitPython_BNO055
 
+#Library adafruit_bno055 must be pip installed on Raspberry Pi
+import adafruit_bno055
+from busio import I2C
+from board import SDA, SCL
+
+i2c = I2C(SCL, SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+# Accessible info: temperature, acceleration, magnetic, gyro, euler, quaternion, linear_acceleration, and gravity
+# via sensor.____
+
+# To calibrate/configure?  offsets_accelerometer, offsets_magnetometer, offsets_gyroscope, 
+# radius_accelerometer, radius_magnetometer
