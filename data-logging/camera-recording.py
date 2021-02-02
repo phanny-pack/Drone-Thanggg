11ae2fae7e980499fc2e4ebee994b4520a4cdf3f
phanny-pack <chrisphan1974@gmail.com>
2021-02-01 17:48:47 -0800

    Confirmed camera recording works on RPi with Kayla! :)

--- data-logging/camera-recording.py
+++ data-logging/camera-recording.py
@@ -3,12 +3,13 @@ import picamera
 
 # Set up the camera
 camera = picamera.PiCamera()
-camera.resolution = ()
+camera.resolution = (640, 480)
 
 # Record and save video
 # We can save the video to the sd card by making a directory
-video_path = # path to directory + name of file
-video_time = # time we want to record for
+video_path = '/home/pi/Videos/test_video_480.h264' # path to directory + name of file
+video_time = 10 # time we want to record for
 camera.start_recording(video_path)
 camera.wait_recording(video_time)
 camera.stop_recording()
+print("done")
