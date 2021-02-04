b80031d56ecb6a934b02c0e5714cab87cc3112ee
Timothy Woo <twoo7@cs.washington.edu>
2021-02-04 10:35:40 -0800

    Add temp code for gps system

--- recovery-system.py
+++ recovery-system.py
@@ -1,22 +1,55 @@
-Process Starts after x minutes of flight
+# Process Starts after x minutes of flight
 
-With GGA Output
-If (GPS Altitude % 10 == Previous State)
-   if (Timer == 20 seconds)
-      Stop other functions();
-      Start Sending Data Through XBee();
-      break from loops
-   else
-      continue;
-Else
-   Previous State = GPS Altitude % 10
-   Reset Timer
+# With GGA Output
+# If (int(GPS Altitude) == Previous State)
+#    if (Timer == 20 seconds)
+#       Stop other functions();
+#       Start Sending Data Through XBee();
+#       break from loops
+#    else
+#       continue;
+# Else
+#    Previous State = GPS Altitude % 10
+#    Reset Timer
 
 
-With RMC Output
-If (Velocity == 0)
-    Stop other functions
-    Start Sending Data Through XBee
-    break from loop
-Else
-   continue checking
+# With RMC Output
+# If (Velocity == 0)
+#     Stop other functions
+#     Start Sending Data Through XBee
+#     break from loop
+# Else
+#    continue checking
+
+
+
+
+#Reporting GPS to Terminal
+import gps
+ 
+# Listen on port 2947 (gpsd) of localhost
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+ 
+while True:
+    try:
+        report = session.next()
+        # Wait for a 'TPV' report and display the current time
+        if report['class'] == 'TPV':
+            if hasattr(report, 'speed'):
+                print(report.speed)
+            if hasattr(report, 'lon'):
+                print(report.lon)
+            if hasattr(report, 'lat'):
+                print(report.lat)
+            if hasattr(report, 'alt'):
+                print(report.alt)
+    except KeyError:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    except StopIteration:
+        session = None
+        print("GPSD has terminated")
+
+
