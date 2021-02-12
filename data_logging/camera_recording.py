d7023f65a8d037dc9982cf209515f2a6e1f0cc9f
phanny-pack <chrisphan1974@gmail.com>
2021-02-11 19:12:09 -0800

    packaged camera recording into a class, and integrated into simulation code. Need to work on parallel running to record at the same time as simulation/control processing

--- data_logging/camera_recording.py
+++ data_logging/camera_recording.py
@@ -1,16 +1,23 @@
 # Note that we will need to install picamera on the Raspberry Pi 
-import picamera
+class camera:
+    def __init__(self, x, y):
+        import picamera
 
-# Set up the camera
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+        # Set up the camera
+        self.x = x
+        self.y = y
+        camera = picamera.PiCamera()
+        camera.resolution = (x, y)
+        # camera.resolution = (640, 480) # best quality, 60 fps
+        # camera.resolution = (1280, 720) # 30 fps
+        # camera.resolution = (1920, 1080) # 30 fps
 
-# Record and save video
-# We can save the video to the sd card by making a directory
-video_path = '/home/pi/Videos/test_video_480.h264' # path to directory + name of file
-video_time = 10 # time we want to record for
-camera.start_recording(video_path)
-print("camera start")
-camera.wait_recording(video_time)
-camera.stop_recording()
-print("camera done")
+        # Record and save video
+        # We can save the video to the sd card by making a directory
+        video_path = '/home/pi/Videos/test_video_480p.h264' # path to directory + name of file
+        video_time = 60 # time we want to record for
+        camera.start_recording(video_path)
+        print("camera start")
+        camera.wait_recording(video_time)
+        camera.stop_recording()
+        print("camera done")
