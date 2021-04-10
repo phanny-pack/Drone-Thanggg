c296b444710b5d455d1f4c88b7bc7668897b3fd9
Kayla <kaylah18@cs.washington.edu>
2021-04-10 14:32:06 -0700

    moved recording code to own function for threading

--- data_logging/camera_recording.py
+++ data_logging/camera_recording.py
@@ -2,7 +2,6 @@
 class camera:
     def __init__(self, x, y):
         import picamera
-
         # Set up the camera
         self.x = x
         self.y = y
@@ -13,14 +12,18 @@ class camera:
         
         # camera.resolution = (640, 480) # best quality, 60 fps
         # camera.resolution = (1280, 720) # 30 fps
-        # camera.resolution = (1920, 1080) # 30 fps
+        # camera.resolution = (1920, 1080) # 30 fps        
 
-        # Record and save video
+    """
+    Records a video for a given time and saves in file_path
+    """
+    # Note: moved to a separate function so we can use the threading specifically for recording
+    def record_video(self, file_path, time):
         # We can save the video to the sd card by making a directory
-        video_path = '/home/pi/Videos/test_video_480p.h264' # path to directory + name of file
-        video_time = 60 # time we want to record for
-        camera.start_recording(video_path)
+        # video_path = '/home/pi/Videos/test_video_480p.h264' # path to directory + name of file
+        # video_time = 60 # time we want to record for
+        camera.start_recording(file_path)
         print("camera start")
-        camera.wait_recording(video_time)
+        camera.wait_recording(time)
         camera.stop_recording()
         print("camera done")
