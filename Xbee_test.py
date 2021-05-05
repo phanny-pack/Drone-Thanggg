a3068e1d3b11f3580e1ce276e45c1d8ec6c6e251
phanny-pack <chrisphan1974@gmail.com>
2021-05-04 17:00:08 -0700

    Various sensor test files

--- Xbee_test.py
+++ Xbee_test.py
@@ -1,6 +1,10 @@
-from digi.xbee.devices import XBeeDevice
+import os
 
-receiver = XBeeDevice("COM7", 9600)
+from digi.xbee.devices import XBeeDevice, DigiMeshDevice, RemoteXBeeDevice
+os.system("sudo systemctl stop serial-getty@serial0.service")
+
+receiver = XBeeDevice("/dev/serial0", 9600)
+receiver.close()
 receiver.open()
 while(1):
     print(receiver.read_data())
