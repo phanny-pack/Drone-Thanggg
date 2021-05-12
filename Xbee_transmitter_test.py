c5d09267132bf0e43914b656c023174cb5ead3fd
phanny-pack <chrisphan1974@gmail.com>
2021-05-11 20:47:55 -0700

    Xbee transmitter test update

--- Xbee_transmitter_test.py
+++ Xbee_transmitter_test.py
@@ -1,9 +1,12 @@
 from digi.xbee.devices import XBeeDevice, XBee64BitAddress, RemoteXBeeDevice
+import time
 
 device = XBeeDevice("COM7", 9600)
 device.open()
-remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20041C&BFFC"))
+device.set_sync_ops_timeout(10)
+remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20041C7BFFC"))
 while(1):
     device.send_data(remote_device, "Data")
     print("sent")
+    time.sleep(3)
 device.close()
